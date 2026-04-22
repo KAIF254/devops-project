5c171ddee90500c2ad505847b015db23ef32e92c
kaif shaikh <ks6225974@gmail.com>
2026-04-22 10:11:34 +0530

    devops project

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Data received" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});

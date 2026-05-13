2f0cb0cb9e7480d6394181b6083cc810bf613177
kaif shaikh <156075872+KAIF254@users.noreply.github.com>
2026-05-13 09:31:18 +0530

    Refactor login logic and remove signup functionality
    Removed temporary user database and signup logic. Updated login logic to allow admin access with hardcoded credentials.

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// Temporary DB (memory)
-let users = [];
-
-// SIGNUP
-app.post("/signup", (req, res) => {
-  const { username, password } = req.body;
-
-  const userExists = users.find(u => u.username === username);
-
-  if (userExists) {
-    return res.json({ message: "User already exists ❌" });
-  }
-
-  users.push({ username, password });
-  res.json({ message: "Signup successful ✅" });
-});
-
-// LOGIN
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  const user = users.find(
-    u => u.username === username && u.password === password
-  );
-
-  if (user) {
-    res.json({ message: "Login successful ✅" });
+  if(username === "admin" && password === "1234") {
+    res.json({ message: "Login Successful" });
   } else {
-    res.json({ message: "Invalid credentials ❌" });
+    res.status(401).json({ message: "Invalid Credentials" });
   }
 });
 
